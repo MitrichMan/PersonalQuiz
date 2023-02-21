27e56a40e94d0f9e4c16b992cc530843477b349a
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-02-21 12:23:12 +0800

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 19.02.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet var resultImageLabel: UILabel!
+    @IBOutlet var resultDescriptionLabel: UILabel!
+    
+    var answers: [Answer] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        
+        let resultAnimal = getMostOftenAnimalType()
+        resultImageLabel.text = "Вы - \(resultAnimal?.rawValue ?? "🐙")"
+        resultDescriptionLabel.text = resultAnimal?.definition ?? "Вы осьминог!"
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+}
+
+private extension ResultViewController {
+    
+    func getMostOftenAnimalType() -> Animal? {
+        var animalsCount: [Animal: Int] = [:]
+        
+        answers.forEach { answer in
+            animalsCount[answer.animal] = (animalsCount[answer.animal] ?? 0) + 1
+        }
+        return animalsCount.max { $0.value < $1.value}?.key
+    }
+}
